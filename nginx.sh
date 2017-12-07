56e4a3471fdd975807e57eeaab6df8d4fdd7a312
Linzhongren <6215714@qq.com>
2017-12-07 09:50:41 +0800

    php-fpm和nginx配合使用

--- nginx.sh
+++ nginx.sh
@@ -40,18 +40,7 @@ server{
    }
 }
 
-## 代理设置
-server{
-   location / {
-      # 所有访问此地址的请求都被转发至 http://localost:8000,即一下server块
-      proxy_pass http://localhost:8000;
-   }
-   # 正则表达式匹配,以~\.开头
-   # 如多个location是正则匹配,以最早匹配的前缀为准.
-   location ~\.(gif|jpg|jpeg|png)$ { 
-      root /data;
-   }
-}
+# 监听指定端口的server
 server{
    listen 8000; # server.listen指令设置监听的端口,若未设置则默认为80
    root /data/upl; # 若location中没有root指令,则使用server中的root指令
@@ -59,6 +48,83 @@ server{
    }
 }
 
+## 代理设置
+server{
+   location / {
+      # 所有访问此地址的请求都被转发至 http://localost:8000,即一下server块
+      proxy_pass http://localhost:8000;
+      # 代理设置(详细请查看 proxy.conf部分，附在文末)
+      proxy_connect_timeout 300s;
+      ...
+   }
+   # 正则表达式匹配,以~\.开头
+   # 如多个location是正则匹配,以最早匹配的前缀为准.
+   location ~\.(gif|jpg|jpeg|png)$ { 
+      root /data;
+   }
+}
+
+## fastcgi设置
+server{
+   location ~\.php$ {
+      root /data/www;
+      fastcgi_pass location:9000;  # php-fpm进程或其它fastcgi实现。
+      fastcgi_index index.php;
+      fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+      fastcgi_param QUERY_STRING $query_string;
+      ... # 更多fastcgi_params的设置，在fastcgi_params.conf中(附在文末)
+   }
+   location ~\.(gif|jpg|png)$ {
+      root /data;
+   }
+}
+
+###############################
+## 完整的proxy.conf文件
+proxy_connect_timeout 300s;
+proxy_send_timeout 900;
+proxy_read_timeout 900;
+proxy_buffer_size 32k;
+proxy_buffers 4 64k;
+proxy_busy_buffers_size 128k;
+proxy_redirect off;
+proxy_hide_header Vary;
+proxy_set_header Accept-Encoding '';
+proxy_set_header Referer $http_referer;
+proxy_set_header Cookie $http_cookie;
+proxy_set_header Host $host;
+proxy_set_header X-Real-IP $remote_addr;
+proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+proxy_set_header X-Forwarded-Proto $scheme;
+
+
+########################################################################
+## fastcgi_params.conf
+
+fastcgi_param  QUERY_STRING       $query_string;
+fastcgi_param  REQUEST_METHOD     $request_method;
+fastcgi_param  CONTENT_TYPE       $content_type;
+fastcgi_param  CONTENT_LENGTH     $content_length;
+
+fastcgi_param  SCRIPT_NAME        $fastcgi_script_name;
+fastcgi_param  REQUEST_URI        $request_uri;
+fastcgi_param  DOCUMENT_URI       $document_uri;
+fastcgi_param  DOCUMENT_ROOT      $document_root;
+fastcgi_param  SERVER_PROTOCOL    $server_protocol;
+fastcgi_param  REQUEST_SCHEME     $scheme;
+fastcgi_param  HTTPS              $https if_not_empty;
+
+fastcgi_param  GATEWAY_INTERFACE  CGI/1.1;
+fastcgi_param  SERVER_SOFTWARE    nginx/$nginx_version;
+
+fastcgi_param  REMOTE_ADDR        $remote_addr;
+fastcgi_param  REMOTE_PORT        $remote_port;
+fastcgi_param  SERVER_ADDR        $server_addr;
+fastcgi_param  SERVER_PORT        $server_port;
+fastcgi_param  SERVER_NAME        $server_name;
+
+# PHP only, required if PHP was built with --enable-force-cgi-redirect
+fastcgi_param  REDIRECT_STATUS    200;
 
 
 
---
+++ php-fpm.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+## 安装和启动
+# 编译方式安装php时，应添加编辑选项 --enable-fpm, 否则安装后没有php-fpm服务端可执行文件
+# 运行php-fpm: 
+#    php编译成功后，php-fpm的可执行文件通常在 $prefix/sbin/php-fpm中
+#    其配置文件通常$prefix/etc/php-fpm.conf
+sbin/php-fpm -c etc/php-fpm.conf # 可启动php-fpm守护进程，使用netstat -ptln查看9000端口
+# 继续配置nginx的fastcgi设置即可。
+
+
+
+exit 0
